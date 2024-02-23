50232180058ebb433b4926ff7b1b0a31c0caf4fe
ethernalarts <51105148+ethernalarts@users.noreply.github.com>
2024-02-23 20:59:58 +0100

    moved settings folder to coopformapp

--- manage.py
+++ manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coopformapp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coopformapp.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
